4e9497a36aabc48eb65088671aa8b30dbed730f3
Roman Joost <romanofski@gimp.org>
2010-02-14 17:33:44 +1000

    Use a general id for two sorts of bugs.

--- src/metatracker/bug.py
+++ src/metatracker/bug.py
@@ -7,6 +7,7 @@ class ExternalBug(object):
 
     zope.interface.implements(metatracker.interfaces.IExternalBug)
 
+    id = ''
     url = ''
     status = ''
     component = ''
@@ -14,14 +15,15 @@ class ExternalBug(object):
     depends = ''
     blocks = ''
 
-    def __init__(self, title, description):
+    def __init__(self, id, title, description):
+        self.id = self.url = id
         self.title = title
         self.description = description
         self.reported = self.modified = datetime.datetime.today()
 
+    def __repr__(self):
+        return "<%s %s>" % (self.__class__.__name__, self.id)
 
-class Bug(object):
+class Bug(ExternalBug):
 
     zope.interface.implements(metatracker.interfaces.IBug)
-
-    id = ''
