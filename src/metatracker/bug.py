4749fd8ea6516be6a84f48668a844e3e71b896d0
Roman Joost <romanofski@gimp.org>
2010-02-14 17:36:19 +1000

    Added test to verify if the bug object really implements it's interface.

--- src/metatracker/bug.py
+++ src/metatracker/bug.py
@@ -3,9 +3,9 @@ import metatracker.interfaces
 import datetime
 
 
-class ExternalBug(object):
+class Bug(object):
 
-    zope.interface.implements(metatracker.interfaces.IExternalBug)
+    zope.interface.implements(metatracker.interfaces.IBug)
 
     id = ''
     url = ''
@@ -16,14 +16,10 @@ class ExternalBug(object):
     blocks = ''
 
     def __init__(self, id, title, description):
-        self.id = self.url = id
+        self.id = id
         self.title = title
         self.description = description
         self.reported = self.modified = datetime.datetime.today()
 
     def __repr__(self):
         return "<%s %s>" % (self.__class__.__name__, self.id)
-
-class Bug(ExternalBug):
-
-    zope.interface.implements(metatracker.interfaces.IBug)
