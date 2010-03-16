0c9329fb355e10ffb628307e4d44ffdeac1fdf1a
Roman Joost <roman@bromeco.de>
2010-03-16 18:58:22 +1000

    Added WIP of a keydispatcher with an keys event system.

--- src/superorganism/gui/projects.py
+++ src/superorganism/gui/projects.py
@@ -3,7 +3,9 @@ import transaction
 import urwid
 import zope.component
 import zope.component.interfaces
+import zope.component.event
 import zope.schema
+import zope.event
 
 
 class NewProjectForm(superorganism.gui.view.BaseView):
@@ -33,35 +35,12 @@ class NewProjectForm(superorganism.gui.view.BaseView):
             keys = self.screen.get_input()
             widget, pos = self.listbox.get_focus()
 
-            for key in keys:
-                if key == 'window resize':
-                    self.size = self.screen.get_cols_rows()
-                elif key == 'q':
-                    transaction.abort()
-                    app = superorganism.interfaces.IApplication(
-                        self.context.__parent__)
-                    return zope.component.getMultiAdapter(
-                        (app, self.screen), name='dashboard').run()
-                elif key == 's':
-                    transaction.commit()
-                    app = superorganism.interfaces.IApplication(
-                        self.context.__parent__)
-                    return zope.component.getMultiAdapter(
-                        (app, self.screen), name='dashboard').run()
-                elif key == 'up':
-                    self.listbox.keypress(size, key)
-                elif key == 'down':
-                    self.listbox.keypress(size, key)
-                else:
-                    widget, pos = self.listbox.get_focus()
-                    widget.keypress((size[0],), key)
+            adapter = zope.component.getMultiAdapter(
+                (self.screen, self), superorganism.gui.interfaces.IKeyDispatcher)
+            adapter.dispatch_key_events()
 
 
-def widgetFactory(context, field):
-    """Currently simple widget factory to create urwid widgets depending
-       to zope.schema.
-    """
-    # XXX currently it doesn't really matter which urwid widget we
-    # create according to the schema. We expect text input. We have to
-    # create more sophisticated widget and fields for validation tho.
-    return 
+
+@zope.component.adapter(superorganism.gui.interfaces.ICharKeyPressEvent)
+def handle_keypress(event):
+    raise ValueError(superorganism.gui.interfaces.IWidget.providedBy(event.widget))
